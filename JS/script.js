fcfadcee2f037a3ce9cd72a289eb7d59799a3486
abubakarwd <abkk222002@gmail.com>
2025-11-06 23:02:31 +0500

    Somehow responsive

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+ // Menu toggle
+        const menuToggle = document.getElementById('menuToggle');
+        const mainNav = document.getElementById('mainNav');
+        menuToggle.addEventListener('click', () => {
+            mainNav.classList.toggle('open');
+        });
